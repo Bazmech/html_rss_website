b58b77ef477eb6db8d3de721c5d14c69a5dfb91e
Barry Bell <barry.bell@imahome.global>
2025-05-20 23:38:10 +0100

    feat: send data back

--- api/feed.js
+++ api/feed.js
@@ -8,9 +8,9 @@ export default async function handler(request, response) {
         response.setHeader('Content-Type', 'application/xml');
         response.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate');
         
-        return response.status(200).text(data);
+        return response.status(200).send(data);
     } catch (error) {
         console.error('Error fetching RSS feed:', error)
-        return response.status(500).text('Failed to fetch RSS feed');
+        return response.status(500).send('Failed to fetch RSS feed');
     }
   }
